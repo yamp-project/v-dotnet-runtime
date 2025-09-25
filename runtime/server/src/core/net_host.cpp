4fa3f9abcbc6e6d5b328b1cc69ee408b5bf262aa
Lucas <BuddiesTV@outlook.de>
2025-09-25 21:45:14 +0200

    feat: Add NetHost

---
+++ runtime/server/src/core/net_host.cpp
@@ -0,0 +1,102 @@
+#include "net_host.h"
+
+namespace dotnet
+{
+    NetHost::NetHost()
+    {
+    }
+
+    void NetHost::Initialize()
+    {
+        m_Library = std::make_unique<NetHostLibrary>();
+        m_Library->Initialize();
+
+        const char_t **argv = new const char_t *[1];
+        argv[0] = CHAR_T_LITERAL("YAMP.Host.Server.dll");
+
+        m_Library->m_hostFxrInitialize(1, argv, nullptr, &(*m_HostHandle));
+        delete[] argv;
+
+        m_Library->InitializeHost(m_HostHandle.get());
+
+        InitializeDelegates();
+
+        m_Library->m_hostFxrRunApp(m_HostHandle.get());
+    }
+
+    void NetHost::InitializeDelegates()
+    {
+        InitializeDelegate(
+            CHAR_T_LITERAL("YAMP.Host.Server.Host, YAMP.Host.Server"),
+            CHAR_T_LITERAL("OnResourceStart"),
+            reinterpret_cast<void **>(&m_resourceStartDelegate));
+
+        InitializeDelegate(
+            CHAR_T_LITERAL("YAMP.Host.Server.Host, YAMP.Host.Server"),
+            CHAR_T_LITERAL("OnResourceStop"),
+            reinterpret_cast<void **>(&m_resourceStopDelegate));
+
+        InitializeDelegate(
+            CHAR_T_LITERAL("YAMP.Host.Server.Host, YAMP.Host.Server"),
+            CHAR_T_LITERAL("OnTick"),
+            reinterpret_cast<void **>(&m_tickDelegate));
+
+        InitializeDelegate(
+            CHAR_T_LITERAL("YAMP.Host.Server.Host, YAMP.Host.Server"),
+            CHAR_T_LITERAL("OnCoreEvent"),
+            reinterpret_cast<void **>(&m_coreEventDelegate));
+
+        InitializeDelegate(
+            CHAR_T_LITERAL("YAMP.Host.Server.Host, YAMP.Host.Server"),
+            CHAR_T_LITERAL("OnResourceEvent"),
+            reinterpret_cast<void **>(&m_resourceEventDelegate));
+    }
+
+    void NetHost::InitializeDelegate(const char_t *typeName, const char_t *methodName, void **delegatePtr)
+    {
+        m_Library->m_getFunctionPointer(
+            typeName,
+            methodName,
+            UNMANAGEDCALLERSONLY_METHOD, nullptr, nullptr,
+            delegatePtr);
+    }
+
+    void NetHost::Shutdown()
+    {
+    }
+
+    void NetHost::OnResourceStart(SDK_Resource *resource)
+    {
+        assert(m_resourceStartDelegate != nullptr);
+
+        m_resourceStartDelegate(resource);
+    }
+
+    void NetHost::OnResourceStop(SDK_Resource *resource)
+    {
+        assert(m_resourceStopDelegate != nullptr);
+
+        m_resourceStopDelegate(resource);
+    }
+
+    void NetHost::OnTick()
+    {
+        assert(m_tickDelegate != nullptr);
+
+        m_tickDelegate();
+    }
+
+    void NetHost::OnCoreEvent(CoreEventType type, CAnyArray *args)
+    {
+        assert(m_coreEventDelegate != nullptr);
+
+        m_coreEventDelegate(type, args);
+    }
+
+    void NetHost::OnResourceEvent(const char *name, CAnyArray *args)
+    {
+        assert(m_resourceEventDelegate != nullptr);
+
+        m_resourceEventDelegate(name, args);
+    }
+} // namespace dotnet
---
+++ runtime/server/src/core/net_host.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <yamp-sdk/sdk.h>
+#include <memory>
+
+#include "net_host_library.h"
+
+#if defined(_WIN32)
+#ifdef _WCHAR_T_DEFINED
+#define CHAR_T_LITERAL(x) L##x
+#else
+#define CHAR_T_LITERAL(x) reinterpret_cast<const char_t *>(u##x)
+#endif
+#else
+#define CHAR_T_LITERAL(x) x
+#endif
+
+typedef void (*resourceStartDelegate)(SDK_Resource *resource);
+typedef void (*resourceStopDelegate)(SDK_Resource *resource);
+typedef void (*tickDelegate)();
+typedef void (*coreEventDelegate)(CoreEventType type, CAnyArray *args);
+typedef void (*resourceEventDelegate)(const char *name, CAnyArray *args);
+
+namespace dotnet
+{
+    class NetHost
+    {
+    public:
+        explicit NetHost();
+        ~NetHost() = default;
+
+        void Initialize();
+        void Shutdown();
+
+        void OnResourceStart(SDK_Resource *resource);
+        void OnResourceStop(SDK_Resource *resource);
+        void OnTick();
+        void OnCoreEvent(CoreEventType type, CAnyArray *args);
+        void OnResourceEvent(const char *name, CAnyArray *args);
+
+    private:
+        std::unique_ptr<NetHostLibrary> m_Library;
+        std::unique_ptr<hostfxr_handle> m_HostHandle = nullptr;
+
+        resourceStartDelegate m_resourceStartDelegate = nullptr;
+        resourceStopDelegate m_resourceStopDelegate = nullptr;
+        tickDelegate m_tickDelegate = nullptr;
+        coreEventDelegate m_coreEventDelegate = nullptr;
+        resourceEventDelegate m_resourceEventDelegate = nullptr;
+
+        void InitializeDelegates();
+        void InitializeDelegate(const char_t *typeName, const char_t *methodName, void **delegatePtr);
+    };
+} // namespace dotnet
