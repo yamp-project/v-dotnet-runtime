1fafb7a086a7697cf1772c786da9f28d6ad83a36
Lucas <BuddiesTV@outlook.de>
2025-09-26 16:03:41 +0200

    feat(module): initial work on host

---
+++ module/YAMP.Host.Server/Host.cs
@@ -0,0 +1,58 @@
+﻿using System.Runtime.InteropServices;
+using YAMP.Shared.Enums;
+using YAMP.Shared.Services;
+using YAMP.Shared.Structs;
+
+namespace YAMP.Host.Server;
+
+internal static class Host
+{
+    private static ResourceService _resourceService = null!;
+
+    private static void Main()
+    {
+        _resourceService = new ResourceService();
+    }
+
+    [UnmanagedCallersOnly]
+    private static unsafe void OnResourceStart(SdkResource* resourcePtr)
+    {
+        var sdkResource = Marshal.PtrToStructure<SdkResource>((IntPtr)resourcePtr);
+
+        var resource = _resourceService.RegisterResource(sdkResource);
+        _resourceService.LoadWrapperForResource(resource);
+
+        resource.LoadAssembly();
+    }
+
+    [UnmanagedCallersOnly]
+    private static unsafe void OnResourceStop(SdkResource* resourcePtr)
+    {
+        var resource = Marshal.PtrToStructure<SdkResource>((IntPtr)resourcePtr);
+    }
+
+    [UnmanagedCallersOnly]
+    private static void OnTick()
+    {
+        foreach (var resource in _resourceService.Resources)
+        {
+            foreach (var tick in resource.Ticks)
+            {
+                tick();
+            }
+        }
+    }
+
+    [UnmanagedCallersOnly]
+    private static unsafe void OnCoreEvent(CoreEventType type, CAnyArray* argsPtr)
+    {
+        var args = Marshal.PtrToStructure<CAnyArray>((IntPtr)argsPtr);
+    }
+
+    [UnmanagedCallersOnly]
+    private static unsafe void OnResourceEvent(char* namePtr, CAnyArray* argsPtr)
+    {
+        var name = Marshal.PtrToStringAnsi((IntPtr)namePtr);
+        var args = Marshal.PtrToStructure<CAnyArray>((IntPtr)argsPtr);
+    }
+}
