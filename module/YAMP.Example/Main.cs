f729923e11e63e1baca22be51f63dce9ed2d95d2
Lucas <BuddiesTV@outlook.de>
2025-09-26 16:03:51 +0200

    feat(module): initial work on example

---
+++ module/YAMP.Example/Main.cs
@@ -0,0 +1,16 @@
+﻿using YAMP.Shared.Core;
+
+namespace YAMP.Example;
+
+public class Main : YampResource
+{
+    public override void OnStart()
+    {
+
+    }
+
+    public override void OnStop()
+    {
+        throw new NotImplementedException();
+    }
+}
