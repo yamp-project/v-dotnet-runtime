4199c0be039faa52aeb252d106e51dfb844282c3
Lucas <BuddiesTV@outlook.de>
2025-09-27 21:44:24 +0200

    feat(module): intercept console output

---
+++ module/YAMP.Wrapper/Writers/InterceptWriter.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using System.Text;
+using YAMP.Shared.Core;
+
+namespace YAMP.Wrapper.Writers;
+
+public class InterceptWriter : TextWriter
+{
+    public override Encoding Encoding => Encoding.UTF8;
+
+    public override void WriteLine(string? value)
+    {
+        if (value == null)
+        {
+            return;
+        }
+
+        SharedLibrary.LogInfo(value);
+    }
+
+    public override void Write(string? value)
+    {
+        if (value == null)
+        {
+            return;
+        }
+
+        SharedLibrary.LogInfo(value);
+    }
+}
