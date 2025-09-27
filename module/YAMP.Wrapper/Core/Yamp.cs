7f9bcf338f175b14ddc0840b03b46812f42e865b
Lucas <BuddiesTV@outlook.de>
2025-09-27 22:14:35 +0200

    chore(module): expose functions to the public

--- module/YAMP.Wrapper/Core/Yamp.cs
+++ module/YAMP.Wrapper/Core/Yamp.cs
@@ -1,4 +1,5 @@
-﻿using YAMP.Shared.Interfaces;
+﻿using YAMP.Shared.Core;
+using YAMP.Shared.Interfaces;
 
 namespace YAMP.Wrapper.Core;
 
@@ -9,5 +10,25 @@ public static class Yamp
     public static void OnTick(Action tick)
     {
         Resource.RegisterTick(tick);
+    }
+
+    public static void LogDebug(string message)
+    {
+        SharedLibrary.LogDebug(message);
+    }
+
+    public static void LogWarning(string message)
+    {
+        SharedLibrary.LogWarn(message);
+    }
+
+    public static void Log(string message)
+    {
+        SharedLibrary.LogInfo(message);
+    }
+
+    public static void LogError(string message)
+    {
+        SharedLibrary.LogError(message);
     }
 }
