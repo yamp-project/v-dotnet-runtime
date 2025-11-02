d316d2228d0397961385e4555929e4cd595870a3
Lucas <BuddiesTV@outlook.de>
2025-11-02 02:29:31 +0100

    feat(interp): add CArray & CDict

---
+++ module/YAMP.Shared/Structs/CArray.cs
@@ -0,0 +1,43 @@
+﻿using System.Runtime.InteropServices;
+using YAMP.Shared.Enums;
+
+namespace YAMP.Shared.Structs;
+
+[StructLayout(LayoutKind.Sequential)]
+public struct CArray
+{
+    public byte type;
+    public UIntPtr size;
+    public IntPtr buffer;
+
+    private IntPtr[] GetBufferPointers()
+    {
+        if (buffer == IntPtr.Zero || size == UIntPtr.Zero)
+        {
+            return [];
+        }
+
+        var length = (int)size;
+        var pointers = new IntPtr[length];
+        for (var i = 0; i < length; i++)
+        {
+            pointers[i] = Marshal.ReadIntPtr(buffer, i * IntPtr.Size);
+        }
+
+        return pointers;
+    }
+
+    public CAnyValue[] GetValues()
+    {
+        var pointers = GetBufferPointers();
+        var values = new CAnyValue[pointers.Length];
+        for (var i = 0; i < pointers.Length; i++)
+        {
+            values[i] = Marshal.PtrToStructure<CAnyValue>(pointers[i]);
+        }
+
+        return values;
+    }
+
+    public CType ValueType => (CType)type;
+}
---
+++ module/YAMP.Shared/Structs/CDict.cs
@@ -0,0 +1,47 @@
+﻿using System.Runtime.InteropServices;
+
+namespace YAMP.Shared.Structs;
+
+[StructLayout(LayoutKind.Sequential)]
+public struct CDict
+{
+    public UIntPtr size;
+    public IntPtr keys;
+    public IntPtr values;
+
+    public string[] GetKeys()
+    {
+        if (keys == IntPtr.Zero || size == UIntPtr.Zero)
+        {
+            return [];
+        }
+
+        var length = (int)size;
+        var result = new string[length];
+        for (var i = 0; i < length; i++)
+        {
+            var ptr = Marshal.ReadIntPtr(keys, i * IntPtr.Size);
+            result[i] = Marshal.PtrToStringAnsi(ptr) ?? string.Empty;
+        }
+
+        return result;
+    }
+
+    public CAnyValue[] GetValues()
+    {
+        if (values == IntPtr.Zero || size == UIntPtr.Zero)
+        {
+            return [];
+        }
+
+        var length = (int)size;
+        var result = new CAnyValue[length];
+        for (var i = 0; i < length; i++)
+        {
+            var ptr = Marshal.ReadIntPtr(values, i * IntPtr.Size);
+            result[i] = Marshal.PtrToStructure<CAnyValue>(ptr);
+        }
+
+        return result;
+    }
+}
