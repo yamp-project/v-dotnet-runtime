1ca42a3b38944d9fc2a4a941ee92b95a2689e6c6
Lucas <BuddiesTV@outlook.de>
2025-11-02 04:53:23 +0100

    chore(module): update example

--- module/YAMP.Example/Main.cs
+++ module/YAMP.Example/Main.cs
@@ -1,4 +1,7 @@
-﻿using YAMP.Shared.Core;
+﻿using YAMP.Shared.Attributes;
+using YAMP.Shared.Core;
+using YAMP.Shared.Enums;
+using YAMP.Wrapper.Core;
 
 namespace YAMP.Example;
 
@@ -6,7 +9,28 @@ public class Main : YampResource
 {
     public override void OnStart()
     {
+        Console.WriteLine(Yamp.Resource.Name);
 
+        Yamp.OnTick(Tick);
+        Yamp.ScanClassInstanceAttributes(this);
+    }
+
+    private void Tick()
+    {
+    }
+
+    [CoreEvent(CoreEventType.PlayerConnect)]
+    private void OnPlayerConnect(int player, string someValue, int someInt)
+    {
+        var a = someInt * 3.14159;
+        var b = Math.Sqrt(a) + Math.Pow(a, 2);
+        var c = Math.Sin(b) + Math.Cos(a);
+        var d = (a + b + c) / 3.0;
+
+        var rng = new Random(player);
+        var randomFactor = rng.NextDouble() * d;
+
+        var result = Math.Log(Math.Abs(randomFactor) + 1);
     }
 
     public override void OnStop()
